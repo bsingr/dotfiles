5b04d047e5c182a6773d7e762036bbcaff1de807
Jens Bissinger <mail@jens-bissinger.de>
2012-07-17 18:19:21 +0200

    extended sublime text settings

--- install_scripts/sublime_text2.sh
+++ install_scripts/sublime_text2.sh
@@ -1,1 +1,13 @@
+# binary
 ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+
+# rbenv plugin
+cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/
+git clone git://github.com/felipeelias/sublime-text-2-rbenv.git rbenv
+
+# install theme
+cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/
+git clone git://github.com/dpree/alien-autopsy.git Alien\ Autopsy
+
+# install settings
+cp ./resources/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
