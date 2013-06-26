52f239fab0c8a21a81224174ece63b3c981aef6f
Jens Bissinger <mail@jens-bissinger.de>
2013-06-26 13:24:31 +0200

    Revert "Symlink Xcode themes instead of copy"
    This reverts commit d6857644f6c62b13838439958ac059dbf8d775f3.

--- install_scripts/xcode.sh
+++ install_scripts/xcode.sh
@@ -3,4 +3,4 @@
 XCODE_THEME_PATH=$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes/
 
 mkdir -p $XCODE_THEME_PATH
-ln -s "$HOME/dev/dotfiles/install_scripts/resources/dpree.dvtcolortheme" $XCODE_THEME_PATH
+cp "$HOME/dev/dotfiles/install_scripts/resources/dpree.dvtcolortheme" $XCODE_THEME_PATH
