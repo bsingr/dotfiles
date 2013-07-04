38bb2ed5c5f0d9c1dc3ae0ba3f75c463b6d9e6d4
Jens Bissinger <mail@jens-bissinger.de>
2013-07-04 15:45:14 +0200

    Replace color theme with the iOS 7 color theme

--- install_scripts/xcode.sh
+++ install_scripts/xcode.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-THEME_FILE="dpree.dvtcolortheme"
-SOURCE_PATH="$HOME/dev/dotfiles/install_scripts/resources/"
-XCODE_THEME_PATH="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes/"
-
-if [ -z "$1" ]; then
-   mkdir -p $XCODE_THEME_PATH
-   cp "$SOURCE_PATH/dpree.dvtcolortheme" $XCODE_THEME_PATH
-else
-   cp "$XCODE_THEME_PATH/dpree.dvtcolortheme" $SOURCE_PATH
-fi
+# install ios7 theme
+curl https://raw.github.com/dpree/xcode-ios7-colors/master/install.sh | sh
