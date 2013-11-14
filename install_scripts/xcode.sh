a3fae2cfa5a292cb83422bde33080e7bb59eb55b
dpree <dpree@bsngr.local>
2013-11-14 21:42:10 +0100

    Fix xcode theme

--- install_scripts/xcode.sh
+++ install_scripts/xcode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # install ios7 theme
-curl https://raw.github.com/dpree/xcode-ios7-colors/master/install.sh | sh
+curl https://raw.github.com/dpree/xcode-ios7-colors/master/iOS%207.dvtcolortheme > "$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes/iOS 7.dvtcolortheme"
