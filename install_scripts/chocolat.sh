4538b263df64ccb60ee0616d327b4cf53eab4293
Jens Bissinger <mail@jens-bissinger.de>
2014-01-19 10:43:56 +0100

    Add chocolat installer.

---
+++ install_scripts/chocolat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MY_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$MY_PATH/lib/_install_plugin.sh"
+
+PACKAGES="$HOME/Library/Application Support/Chocolat/TextmateBundles"
+
+# plugins
+install_plugin "git://github.com/slim-template/ruby-slim.tmbundle.git" "Ruby-Slim"
