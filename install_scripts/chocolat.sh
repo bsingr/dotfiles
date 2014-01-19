b201ebf5ecd8f7f689b15659d05c5163e10a1a26
Jens Bissinger <mail@jens-bissinger.de>
2014-01-19 11:01:11 +0100

    Fix chocolat.sh.

--- install_scripts/chocolat.sh
+++ install_scripts/chocolat.sh
@@ -6,4 +6,5 @@ source "$MY_PATH/lib/_install_plugin.sh"
 PACKAGES="$HOME/Library/Application Support/Chocolat/TextmateBundles"
 
 # plugins
-install_plugin "git://github.com/slim-template/ruby-slim.tmbundle.git" "Ruby-Slim"
+install_plugin "git://github.com/slim-template/ruby-slim.tmbundle.git" "ruby-slim.tmbundle"
+install_plugin "git@github.com:textmate/ini.tmbundle.git" "ini.tmbundle"
