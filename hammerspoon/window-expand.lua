b699292bf371d8c1773883de3fb30fed2fb7b062
Jens Bissinger <mail@jens-bissinger.de>
2017-04-03 11:04:21 +0200

    Add hyper+delete (backspace) to expand active iterm pane

--- hammerspoon/window-expand.lua
+++ hammerspoon/window-expand.lua
@@ -1,3 +1,17 @@
+local function pressFn(mods, key)
+  if key == nil then
+    key = mods
+    mods = {}
+  end
+  return function() hs.eventtap.keyStroke(mods, key, 1000) end
+end
+
+local function remap(mods, key, pressFn)
+  hs.hotkey.bind(mods, key, pressFn, nil, pressFn)
+end
+
+remap({"cmd", "alt", "ctrl", "shift"}, "delete", pressFn({"cmd", "shift"}, "Return"))
+
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "Return", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
