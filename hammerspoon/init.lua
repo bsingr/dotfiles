300ed557260d37891b5accdf382b2e920012e76d
Jens Bissinger <mail@jens-bissinger.de>
2021-04-13 16:13:01 +0200

    Refactor

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,17 +1,14 @@
---
 -- Hammerspoon Config for Hyper-Capslock-Key
 -- (Karabiner-Elements required)
 -- (c) 2014-2021 bsingr
---
 
 -- disable slowish animations
 hs.window.animationDuration = 0
 
+-- define hyper
 HYPER_KEY = {"cmd", "alt", "ctrl", "shift"}
 
---
--- window expand
---
+-- [Use-case] Expand focused window to full-screen
 hs.hotkey.bind(HYPER_KEY, "Return", function()
   hs.window.focusedWindow():setFrame(hs.window.focusedWindow():screen():frame())
 end)
@@ -19,9 +16,58 @@ hs.hotkey.bind(HYPER_KEY, "F", function()
   hs.window.focusedWindow():toggleFullScreen()
 end)
 
---
--- cycle through all windows of the current application
---
+-- [Use-case] Move focused window to specific screen
+-- http://macbiblioblog.blogspot.de/2014/12/key-codes-for-function-and-special-keys.html
+hs.hotkey.bind(HYPER_KEY, 18, function() -- screen key 1 = keycode 18
+  hs.window.focusedWindow():move(hs.screen.allScreens()[1]:frame(), screen)
+end)
+hs.hotkey.bind(HYPER_KEY, 19, function() -- screen key 2 = keycode 19
+  hs.window.focusedWindow():move(hs.screen.allScreens()[2]:frame(), screen)
+end)
+hs.hotkey.bind(HYPER_KEY, 20, function() -- screen key 3 = keycode 20
+  hs.window.focusedWindow():move(hs.screen.allScreens()[3]:frame(), screen)
+end)
+
+-- [Use-case] Move focused window within current screen
+hs.hotkey.bind(HYPER_KEY, "Up", function()
+  local f = hs.window.focusedWindow():screen():frame()
+  f.h = f.h / 2
+  hs.window.focusedWindow():setFrame(f)
+end)
+hs.hotkey.bind(HYPER_KEY, "Down", function()
+  local f = hs.window.focusedWindow():screen():frame()
+  f.y = f.h / 2 + f.y
+  f.h = f.h / 2
+  hs.window.focusedWindow():setFrame(f)
+end)
+hs.hotkey.bind(HYPER_KEY, "Left", function()
+  local f = hs.window.focusedWindow():screen():frame()
+  f.w = f.w / 2
+  hs.window.focusedWindow():setFrame(f)
+end)
+hs.hotkey.bind(HYPER_KEY, "Right", function()
+  local f = hs.window.focusedWindow():screen():frame()
+  f.x = f.w / 2
+  f.w = f.w / 2
+  hs.window.focusedWindow():setFrame(f)
+end)
+
+-- [Use-case] Launch or focus specific Application
+local function bindAppSwitch(key, appName)
+  hs.hotkey.bind(HYPER_KEY, key, function()
+    hs.application.launchOrFocus(appName)
+  end)  
+end
+bindAppSwitch("G", "Tower")
+bindAppSwitch("T", "iTerm")
+bindAppSwitch("C", "Microsoft Teams")
+bindAppSwitch("X", "Xcode")
+bindAppSwitch("E", "Visual Studio Code")
+bindAppSwitch("B", "Google Chrome")
+bindAppSwitch("N", "Messages")
+bindAppSwitch("M", "Mail")
+
+-- [Use-case] Cycle through all windows of focused application
 hs.hotkey.bind(HYPER_KEY, "space", function()
   local app = hs.application.frontmostApplication()
   local current = 1
@@ -36,67 +82,3 @@ hs.hotkey.bind(HYPER_KEY, "space", function()
   end
   app:allWindows()[next]:focus()
 end)
-
---
--- window move screens
---
--- http://macbiblioblog.blogspot.de/2014/12/key-codes-for-function-and-special-keys.html
--- no 1 = keycode 18
-hs.hotkey.bind(HYPER_KEY, 18, function()
-  hs.window.focusedWindow():move(hs.screen.allScreens()[1]:frame(), screen)
-end)
--- no 2 = keycode 19
-hs.hotkey.bind(HYPER_KEY, 19, function()
-  hs.window.focusedWindow():move(hs.screen.allScreens()[2]:frame(), screen)
-end)
--- no 3 = keycode 20
-hs.hotkey.bind(HYPER_KEY, 20, function()
-  hs.window.focusedWindow():move(hs.screen.allScreens()[3]:frame(), screen)
-end)
-
---
--- window move onscreen position
---
-hs.hotkey.bind(HYPER_KEY, "Up", function()
-  local win = hs.window.focusedWindow()
-  local f = win:screen():frame()
-  f.h = f.h / 2
-  win:setFrame(f)
-end)
-hs.hotkey.bind(HYPER_KEY, "Down", function()
-  local win = hs.window.focusedWindow()
-  local f = win:screen():frame()
-  f.y = f.h / 2 + f.y
-  f.h = f.h / 2
-  win:setFrame(f)
-end)
-hs.hotkey.bind(HYPER_KEY, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:screen():frame()
-  f.w = f.w / 2
-  win:setFrame(f)
-end)
-hs.hotkey.bind(HYPER_KEY, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:screen():frame()
-  f.x = f.w / 2
-  f.w = f.w / 2
-  win:setFrame(f)
-end)
-
---
--- application switcher
---
-local function bindAppSwitch(key, appName)
-  hs.hotkey.bind(HYPER_KEY, key, function()
-    hs.application.launchOrFocus(appName)
-  end)  
-end
-bindAppSwitch("G", "Tower")
-bindAppSwitch("T", "iTerm")
-bindAppSwitch("C", "Microsoft Teams")
-bindAppSwitch("X", "Xcode")
-bindAppSwitch("E", "Visual Studio Code")
-bindAppSwitch("B", "Google Chrome")
-bindAppSwitch("N", "Messages")
-bindAppSwitch("M", "Mail")
